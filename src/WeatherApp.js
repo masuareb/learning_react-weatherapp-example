786f158a1dd9a961be627a9c6314a687c8752f8e
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 13:11:27 -0600

    Example of fetch

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -7,7 +7,7 @@ class WeatherApp extends Component {
         this.state = {temp: 0, desc: '', icon: '', loading: true}
     }
     componentDidMount() {
-        fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&apiKEY=18f323e4e2ec7cc1acd6bd932f548bd0')
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&apiKEY=APIKEY')
             .then(response => response.json())
             .then(responseData => {
                 this.setState({
