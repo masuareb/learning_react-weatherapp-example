61dda4e0cd21bce2865f8212d5e8c365738b867d
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 12:23:21 -0600

    Create component WeatherApp

---
+++ src/WeatherApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class WeatherApp extends component {
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
+}
+export default WeatherApp;
