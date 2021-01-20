120e82d41afb57d51b220f6e4ba5cc66108f570b
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 12:34:02 -0600

    Fetch request and change component state based on response

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -6,6 +6,21 @@ class WeatherApp extends component {
         super(props);
         this.state = {temp: 0, desc: '', icon: '', loading: true}
     }
+    componentDidMount() {
+        fetch('http://api.openweathermap.org/data/2.5/weather?\
+            q=London&units=Metric\
+            &apiKEY=18f323e4e2ec7cc1acd6bd932f548bd0')
+            .then(response => response.json())
+            .then(responseData => {
+                this.setState({
+                    temp: responseData.main.temp,
+                    desc: responseData.weather[0].description,
+                    icon: responseData.weather[0].icon,
+                    loading: false
+                })
+            })
+            .catch(err => console.error(err));
+    }
     render() {
         return (
             <div className="App">
