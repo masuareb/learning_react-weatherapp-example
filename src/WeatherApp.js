46582d0dd4812f7938288f260bb497e66af7e69e
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 12:29:49 -0600

    Added constructor

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class WeatherApp extends component {
+    constructor(props) {
+        super(props);
+        this.state = {temp: 0, desc: '', icon: '', loading: true}
+    }
     render() {
         return (
             <div className="App">
